4bc3e26490d41b0c6c2dfd889966f0b58f09057f
berkcangurel <berkcangurel@gmail.com>
2017-01-08 20:38:23 +0300

    For each image, probability maps for each object is calculated

--- Testing.m
+++ Testing.m
@@ -3,6 +3,7 @@ addpath(genpath('lib/ncut/'));
 
 clear;
 load('Codebook.mat');
+load('SVM_models')
 
 ObjectCount = 8;
 CodebookSize = size(Codebook, 1);
@@ -12,13 +13,18 @@ GridSize = 32;
 % Get file location data.
 file = fopen('config.txt');
 DataLocation = fgetl(file);
+vlToolboxLocation = fgetl(file);
+run(vlToolboxLocation);
 
 % Partition the dataset into two subsets as Training and Test.
 [~, ~, test_images, test_masks] = partitionDataset(DataLocation);
 
 % Segment each image to detect objects. Compute their Bag of Words
 % representation and use SVM classifier to recognize the objects.
-for imageIndex = 1:size(test_images, 2)
+
+AllProbabilityMaps = cell(size(test_images,2),1);
+
+for imageIndex = 94:94
     
     image = test_images{1, imageIndex};
     grayscale = single(rgb2gray(image));
@@ -60,5 +66,29 @@ for imageIndex = 1:size(test_images, 2)
     % Use SVM classifier to compute the probability of each segment to
     % contain an instance of any object.
     
+    % Calculate the probability for each segment to include an object
+    probability = zeros(SegmentCount, ObjectCount);
     
+    for i=1:SegmentCount
+        for j=1:ObjectCount
+            [~, ~ , prob] = svmpredict([1], SegmentsBagOfWords(i,:), SVM_models{j}, '-b 1');
+            probability(i,j) = prob(1,1);
+        end
+    end
+    
+    ProbabilityMaps = cell(1,ObjectCount);
+    
+    for i=1:ObjectCount
+        current_map = zeros(height,width);
+        for j=1:height
+            for k=1:width
+                current_map(j,k) = probability(SegmentLabels(j,k),i);
+            end
+        end
+        ProbabilityMaps{i} = current_map;
+    end
+    save('ProbabilityMaps','ProbabilityMaps');
+    AllProbabilityMaps{imageIndex,1} = ProbabilityMaps;
 end
+
+
